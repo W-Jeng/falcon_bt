72b5417428f5ed4c45eddf6f095c362325ab6d9f
W-Jeng <woonjeng@gmail.com>
2024-07-21 15:45:46 +0800

    thinking of alternatively to process tick level data, since the standard load all and process is slow

--- src/data_reader.h
+++ src/data_reader.h
@@ -4,52 +4,104 @@
 #include <fstream>
 #include <string>
 #include <filesystem>
+#include <cstdint>
 
-
-struct Person {
-    std::string name;
-    int age;
-    std::string city;
+struct MboMessage{
+    std::string ts_recv;
+    std::string ts_event;
+    uint16_t rtype;
+    uint16_t publisher_id;
+    uint16_t instrument_id;
+    std::string action;
+    std::string side;
+    std::string price;
+    uint32_t size;
+    uint16_t channel_id;
+    uint16_t order_id;
+    uint16_t flags;
+    uint32_t ts_in_delta;
+    uint32_t sequence;
+    std::string symbol;
 };
 
 
 void read_content() {
-    std::ifstream inputFile("C:\\Users\\User\\Desktop\\cpp\\falcon_bt\\src\\sample.txt");
+    std::ifstream input_file("C:\\Users\\User\\Desktop\\cpp\\falcon_bt\\src\\mbo_ge.csv");
 
-    // Check if the file was successfully opened
-    if(!inputFile) {
-        std::cerr << "Unable to open file." << std::endl;
-        return; // Exit with an error code
+    if(!input_file) {
+        std::cout << "Unable to open file." << std::endl;
+        return;
     }
 
     std::string line;
-    std::vector<Person> people;
+    std::vector<MboMessage> mbo_messages;
 
     // Skip the header line
-    std::getline(inputFile,line);
+    std::getline(input_file,line);
 
-    // Read the file line-by-line
-    while(std::getline(inputFile,line)) {
-        std::istringstream lineStream(line);
+    while(std::getline(input_file,line)) {
+        std::istringstream line_stream(line);
         std::string field;
-        Person person;
+        MboMessage mbo_msg;
 
-        // Parse each field
-        std::getline(lineStream,person.name,',');
-        std::getline(lineStream,field,',');
-        person.age = std::stoi(field);
-        std::getline(lineStream,person.city,',');
+        std::getline(line_stream, mbo_msg.ts_recv, ',');
 
-        people.push_back(person);
+        
+        std::getline(line_stream, mbo_msg.ts_event, ',');
+
+        std::getline(line_stream, field,',');
+        mbo_msg.rtype = std::stoi(field);
+
+        std::getline(line_stream, field,',');
+        mbo_msg.publisher_id = std::stoi(field);
+        
+        std::getline(line_stream,field,',');
+        mbo_msg.instrument_id = std::stoi(field);
+
+        std::getline(line_stream, mbo_msg.action,',');
+        std::getline(line_stream, mbo_msg.side,',');
+        
+        std::getline(line_stream, mbo_msg.price,',');
+        
+        std::getline(line_stream,field,',');
+        mbo_msg.size = std::stoi(field);
+        
+        std::getline(line_stream,field,',');
+        mbo_msg.channel_id = std::stoi(field);
+
+        std::getline(line_stream,field,',');
+        mbo_msg.order_id = std::stoi(field);
+
+        std::getline(line_stream,field,',');
+        mbo_msg.flags = std::stoi(field);
+
+        std::getline(line_stream,field,',');
+        mbo_msg.ts_in_delta = std::stoi(field);
+        
+        std::getline(line_stream,field,',');
+        mbo_msg.sequence = std::stoi(field);
+        
+        std::getline(line_stream,mbo_msg.symbol,',');
+        
+        mbo_messages.push_back(mbo_msg);
+
+        if (mbo_messages.size() % 100000 == 0) 
+            std::cout << "size: " << mbo_messages.size() << std::endl;
     }
 
-    // Close the file
-    inputFile.close();
-
-    // Process the data (for example, print it to the console)
-    for(const auto& person : people) {
-        std::cout << "Name: " << person.name << ", Age: " << person.age << ", City: " << person.city << std::endl;
-    }
-
-    return; // Exit successfully
+    input_file.close();
+    
+    return;
 }
+
+/*
+To save memory, it's better to load the tick data on the fly and maintain it using a min_heap on the unix timestamp
+1) initiation requires reading data for string stream across different files for ONE DAY
+*/
+class DataReader {
+
+public:
+
+
+};
+
